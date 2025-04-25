ff226060181e2681278a1b766d13782f59b66082
Damian Williamson <willtech@live.com.au>
2025-04-26 02:03:13 +1000

    bashntpdate.sh Initial Commit
    Signed-off-by: Damian Williamson <willtech@live.com.au>

---
+++ bashntpdate.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# bashntpdate.sh
+#Professor. Damian A. James Williamson Grad.
+#
+
+trap 'echo "User tried to escape ^C"; wait' INT
+
+#Setup
+furstrun=1
+logfile="bashntpdate.log"
+
+
+#Functions
+## New Date
+newdate=(#!/bin/bash
+# Get the DateTime
+#Professor. Damian A. James Williamson Grad.
+#
+date -u +"%H:%M:%S %A %d/%b/%Y +0000h")
+#Use: `"${newdate[@]}"` to return DateTime
+
+##Exit Handler
+Exit_handler() {
+	echo "CTRL+C received."
+	echo "" >> $logfile 
+	"${newdate[@]} Resume Save" >> $logfile
+	echo $i.$j.$k.$l >> $logfile
+	echo "Progress is logged."
+        exit 0
+}
+
+
+#Begin
+"${newdate[@]}"
+if [[ -w $logfile ]]; then
+        echo "Reading starting point..."
+        octetpos=0
+        startip=`tail -1 $logfile`
+        ip route get $startip > /dev/null 2>&1
+        validip=$?
+        if [[ "$[validip]" == 0 ]]; then
+                octets=$(echo $startip | tr "." "\n")
+                for addr in $octets
+                do
+                        octet[$octetpos]=$[addr] ; octetpos=$((octetpos+1))
+                        echo "> [$addr]"
+                done
+
+        else
+                cat $logfile
+                echo "Lofile at $[logfile] failed to record valid IP"
+                exit 0
+        fi
+
+else
+	echo "" > $logfile
+fi
+
+#Main
+trap 'echo ""; trap " " SIGINT SIGTERM SIGHUP; kill 0; wait; Exit_handler' SIGINT SIGTERM SIGHUP
+for ((i="${octet[0]:-0}";i<=255;i++)); do
+	for ((j="${octet[1]:-0}";j<=255;j++)); do
+		for ((k="${octet[2]:-0}";k<=255;k++)); do
+			for ((l="${octet[3]:-0}";l<=255;l++)); do
+				echo "ntpdate -q $i.$j.$k.$l"
+				output=$(ntpdate -q $i.$j.$k.$l 2>&1)
+				if [[ $? = 0 ]]; then
+					echo "" >> $logfile
+					"${newdate[@]}" >> $logfile
+					echo "ntpdate -q $i.$j.$k.$l" >> $logfile
+					echo "$output" >> $logfile
+					echo "" >> $logfile
+					echo "$output"
+				fi
+			done
+		done
+	done
+done
+
+cat $logfile
+
+exit 0
